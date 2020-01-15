6a8ca5a3df88abc6e7694a6f827168b2ff189e67
npnelson <nnelson1@insight.rr.com>
2020-01-15 15:08:15 -0500

    wip

---
+++ src/NETToolBox.RequireHTTPS/RequireHTTPSMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using NETToolBox.RequireHTTPS;
+using System.Threading.Tasks;
+
+namespace NETToolBox.RequireHTTPS
+{
+#pragma warning disable S101 // Types should be named in PascalCase
+    public sealed class RequireHTTPSMiddleware
+#pragma warning restore S101 // Types should be named in PascalCase
+    {
+
+        private readonly RequestDelegate _next;
+        public RequireHTTPSMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.IsHttps)
+            {
+                await _next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;              
+            }
+        }
+    }
+}
+
+namespace Microsoft.AspNetCore.Builder
+{
+#pragma warning disable S101 // Types should be named in PascalCase
+    public static class RequireHTTPSMiddlewareExtensions
+#pragma warning restore S101 // Types should be named in PascalCase
+    {
+        /// <summary>
+        /// Requires HTTPS NO MATTER WHAT!!! Non HTTPS requests will return 400 BadRequest 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder RequireHTTPS(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequireHTTPSMiddleware>();
+        }
+    }
+}
---
+++ test/NETToolBox.RequireHTTPS.Tests/RequireHTTPSMiddlewareTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NETToolBox.RequireHTTPS.Tests
+{
+    public class RequireHTTPSMiddlewareTests
+    {
+        [Fact]
+        public async Task HTTPSRequestCallsNextDelegate()
+        {
+            //Arrange
+            var context = new DefaultHttpContext();
+            context.Request.IsHttps = true;
+          
+            //just add a delegate so we can test to make sure it got called
+            RequestDelegate next = x =>
+            {
+                x.Response.ContentType = "application/xml";  //just picked an arbitrary non default contenttype
+                return Task.CompletedTask;
+            };
+
+            var middleware = new RequireHTTPSMiddleware(next);
+
+            //Act
+            await middleware.Invoke(context);
+
+            //Assert
+
+            context.Response.ContentType.Should().Be("application/xml"); //make sure our delegate got called  
+        }
+
+        [Fact]
+        public async Task HTTPRequestReturnsBadRequest()
+        {
+            //Arrange
+            var context = new DefaultHttpContext();
+            context.Request.IsHttps = false;
+            
+          
+            var middleware = new RequireHTTPSMiddleware(next:(innerHttpContext)=>
+            {
+                return Task.CompletedTask;
+            }
+            );
+
+            //Act
+            await middleware.Invoke(context);
+
+            //Assert
+
+            context.Response.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+    }
+}
