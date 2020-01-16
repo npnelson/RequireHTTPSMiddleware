b6d8c935765d0c5a9ae6a5ac903fe68f144426ae
npnelson <nnelson1@insight.rr.com>
2020-01-16 09:59:47 -0500

    added author to project for nuget and add comment about load balancers

--- src/NETToolBox.RequireHTTPS/RequireHTTPSMiddleware.cs
+++ src/NETToolBox.RequireHTTPS/RequireHTTPSMiddleware.cs
@@ -36,7 +36,9 @@ namespace Microsoft.AspNetCore.Builder
 #pragma warning restore S101 // Types should be named in PascalCase
     {
         /// <summary>
-        /// Requires HTTPS NO MATTER WHAT!!! Non HTTPS requests will return 400 BadRequest 
+        /// Requires HTTPS NO MATTER WHAT!!! Non HTTPS requests will return 400 BadRequest
+        /// When running behind a load balancer/proxy, you may need to enable ForwardedHeaderMiddleware see:https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/proxy-load-balancer?view=aspnetcore-3.1
+        /// also https://devblogs.microsoft.com/aspnet/forwarded-headers-middleware-updates-in-net-core-3-0-preview-6/
         /// </summary>
         /// <param name="builder"></param>
         /// <returns></returns>
